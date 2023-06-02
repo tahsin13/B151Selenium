2bf67c617dcad8d7db943addd8228d66b2a33ccc
Tahsin <tahsinkorkmaz89@gmail.com>
2023-06-02 21:02:34 +0200

    02-06-2023

--- src/test/java/techproed/day05_Xpath_cssSelector/C03_relativeLocators.java
+++ src/test/java/techproed/day05_Xpath_cssSelector/C03_relativeLocators.java
@@ -5,25 +5,23 @@ import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.locators.RelativeLocator;
 
 import java.time.Duration;
 
-import static dev.failsafe.Failsafe.with;
+import static org.openqa.selenium.support.locators.RelativeLocator.with;
 
 public class C03_relativeLocators {
     public static void main(String[] args) {
-        System.setProperty("chromeDriver", "src/main/resources/drivers/chromedriver");
-        WebDriver driver = new ChromeDriver();
-        driver.manage().window().maximize();
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
-    /*
+        /*
         Benzer özelliklere sahip webelementler için relative locator kullanabiliriz
         SYNTAX;
         driver.findElement(with(By.tagname).below().above().to_left_of().to_right_of().near()) gibi
         methotlar ile benzer özelliklere sahip webelementleri locatini almadan o web elemente ulaşabiliriz
          */
-
+        System.setProperty("chromeDriver", "src/main/resources/drivers/chromedriver");
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
         //amazona gidelim
         driver.get("https://amazon.com");
 
@@ -32,9 +30,10 @@ public class C03_relativeLocators {
         aramaKutusu.sendKeys("city bike", Keys.ENTER);
 
         //relative locator kullanarak hybrid bikes altındaki elemente tıklayalım
+        WebElement hybridBike = driver.findElement(By.xpath("//*[text()='Hybrid Bikes']"));
+        WebElement electricBike = driver.findElement(with(By.tagName("a")).below(hybridBike));
+        WebElement completeBike = driver.findElement(with(By.tagName("a")).below(electricBike));
+        completeBike.click();
 
     }
-
-
-
 }
